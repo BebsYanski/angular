d6ad5a0371756b364d59ef6cf753b8ebaadf5b60
BebsYanski <bebongnchuy@gmail.com>
2025-11-01 05:04:53 +0100

    conditional hide and show elements using *ngIf and @if

--- angular_19_proj/src/app/ngif-vs-if/ngif-vs-if.ts
+++ angular_19_proj/src/app/ngif-vs-if/ngif-vs-if.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ngif-vs-if',
   standalone: true, // ✅ important
-  imports: [CommonModule], // ✅ enables *ngIf, *ngFor
+  imports: [CommonModule, FormsModule], // ✅ enables *ngIf, *ngFor
   templateUrl: './ngif-vs-if.html',
   styleUrls: ['./ngif-vs-if.scss'], // ✅ plural
 })
 export class NgifVsIf {
   isChecked: boolean = false;
   isInputBox: boolean = true;
+  input1: string = '';
+  input2: string = '';
 
   onClick() {
     this.isChecked = !this.isChecked;
@@ -18,7 +21,7 @@ export class NgifVsIf {
 
   showField() {
     this.isInputBox = true;
-  } 
+  }
 
   hideField() {
     this.isInputBox = false;
