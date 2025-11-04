394a17cbe06af6f28f5a617f907ba929c5dc5a97
BebsYanski <bebongnchuy@gmail.com>
2025-11-04 07:32:39 +0100

    roundup with attribute directives

--- angular_19_proj/src/app/attribute-directives/attribute-directives.ts
+++ angular_19_proj/src/app/attribute-directives/attribute-directives.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-attribute-directives',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './attribute-directives.html',
   styleUrl: './attribute-directives.scss',
 })
@@ -11,5 +12,23 @@ export class AttributeDirectives {
   textColor: string = 'text-danger';
   changeColor(): void {
     this.textColor = this.textColor === 'text-danger' ? 'text-success' : 'text-danger';
+    this.isTextGreen = !this.isTextGreen;
   }
+
+  isTextGreen: boolean = false;
+  userClass: string = 'text-info';
+
+  styleColor: string = 'orange';
+  updateColor(ngStyleColor: string): any {
+    this.styleColor = ngStyleColor;
+  }
+
+  isTextBlue: boolean = false;
+
+  customClass: any = {
+    color: 'pink',
+    height: '150px',
+    width: '400px',
+    border: '2px solid grey',
+  };
 }
