42965fa7dc3417ae63762194d61ef5e26252b852
BebsYanski <bebongnchuy@gmail.com>
2025-11-12 05:02:57 +0100

    Implement string pipes

--- angular_19_proj/src/app/built-in-pipes/built-in-pipes.ts
+++ angular_19_proj/src/app/built-in-pipes/built-in-pipes.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-built-in-pipes',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './built-in-pipes.html',
-  styleUrl: './built-in-pipes.scss'
+  styleUrl: './built-in-pipes.scss',
 })
 export class BuiltInPipes {
-
+  angularPipes: string = 'Use pipes to format data in angular templates';
+  personData = {
+    name: 'John',
+    age: 30,
+    city: 'Mumbai',
+  };
+  students = [
+    { id: 1, name: 'Yan', age: 33 },
+    { id: 1, name: 'Yan', age: 33 },
+    { id: 1, name: 'Yan', age: 33 },
+  ];
+  currentDate: Date = new Date();
 }
